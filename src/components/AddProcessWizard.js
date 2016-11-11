c093ae1b418d3507e45406f5904f068219a5a519
leitediogo <leitediogo@gmail.com>
2016-11-11 18:49:08 +0000

    wizard form tryout

--- src/components/AddProcessWizard.js
+++ src/components/AddProcessWizard.js
@@ -7,23 +7,45 @@ import {
     Stepper,
     StepLabel,
 } from 'material-ui/Stepper'
-import AddProcessNotification from './AddProcessNotification'
-import AddProcessExceptionHandling from './AddProcessExceptionHandling'
 import TextField from 'material-ui/TextField'
 import Paper from 'material-ui/Paper'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import { Table, TableRow, TableBody, TableRowColumn, TableHeader, TableHeaderColumn } from 'material-ui/Table'
+import IconDelete from 'material-ui/svg-icons/action/delete'
+import Checkbox from 'material-ui/Checkbox';
 
-const paperStyle = {
-    margin: 20,
-    textAlign: 'center'
-}
+const iconDelete = <IconDelete />
 
-const selectStyle = {
-    customWidth: {
-        width: 150
+
+
+const styles = {
+    block: {
+        maxWidth: 250,
+    },
+    checkbox: {
+        marginBottom: 16,
+    },
+    paper: {
+        margin: 20,
+        textAlign: 'center',
+        width: 500,
+        height: 300
     }
-}
+};
+
+const tableData = [
+    {
+        order: '1',
+        type: 'John Smith',
+        name: 'PPO Platform'
+    },
+    {
+        order: '2',
+        type: 'Mathew',
+        name: 'Send Mail'
+    }
+]
 
 class AddProcessWizard extends Component {
 
@@ -38,13 +60,49 @@ class AddProcessWizard extends Component {
                 acronym: '',
                 description: '',
                 type: 1,
+                help: '',
                 status: 'certification',
-                help: ''
+                version: '',
+                isActive: false,
+                supervisorTeam: {
+                    userId: '',
+                    userName: '',
+                    function: ''
+                },
+                supportTeam: {
+                    userId: '',
+                    userName: '',
+                    function: ''
+                },
+                step: {
+                    name: '',
+                    type: '',
+                    permissions: {
+                        userId: '',
+                        userName: '',
+                        function: ''
+                    },
+                    notification: {
+                        supervisorOnError: '',
+                        supervisorOnEnd: ''
+                    },
+                    errorHandling: {
+                        AssigntoSupervisorOnError: false,
+                        blockProcessExecution: false
+                    },
+                },
+                notification: {
+                    supervisorOnError: false,
+                    supervisorOnEnd: false
+                },
+                errorHandling: {
+                    AssigntoSupervisorOnError: false,
+                    blockProcessExecution: false
+                }
             }
         }
         this.handleChange = this.handleChange.bind(this);
     }
-
 
     handleChange(name, e) {
         //TODO: bind select
@@ -54,7 +112,7 @@ class AddProcessWizard extends Component {
         console.log(this.state)
     }
 
-    handleSelectChange = (event, index, value) => this.setState({value});
+    handleSelectChange = (event, index, value) => this.setState({ value });
 
     handleNext = () => {
         const {stepIndex} = this.state;
@@ -76,30 +134,31 @@ class AddProcessWizard extends Component {
             case 0:
                 return (
                     <div>
-                        <Paper zDepth={1} style={paperStyle}>
+                        <Paper zDepth={1} style={styles.paper}>
                             <TextField
                                 hintText="Insert Process Name"
                                 floatingLabelText="Process Name"
                                 value={this.state.processDef.name}
                                 onChange={this.handleChange.bind(this, 'name')}
                                 />
+                            <br />
                             <TextField
                                 hintText="Insert Process Acronym"
                                 floatingLabelText="Process Acronym"
                                 value={this.state.processDef.acronym}
                                 onChange={this.handleChange.bind(this, 'acronym')}
                                 />
+                            <br />
                             <TextField
                                 hintText="Insert Process Description"
                                 floatingLabelText="Process Description"
                                 value={this.state.processDef.description}
                                 onChange={this.handleChange.bind(this, 'description')}
                                 />
+                            <br />
                             <SelectField
-                                floatingLabelText="Type"
                                 value={this.state.value}
                                 onChange={this.handleSelectChange}
-                                style={selectStyle.customWidth}
                                 >
                                 <MenuItem value={1} primaryText="Automated" />
                                 <MenuItem value={2} primaryText="Sequential" />
@@ -113,21 +172,65 @@ class AddProcessWizard extends Component {
             case 1:
                 return (
                     <div>
-                        <Paper zDepth={1} style={paperStyle}>
-                            <TextField
-                                hintText="Insert Step"
-                                floatingLabelText="Step"
-                                value={this.state.processDef.help}
-                                onChange={this.handleChange.bind(this, "help")}
-                                />
-                            <br />
+                        <Paper zDepth={1} style={styles.paper}>
+                            <Table>
+                                <TableHeader displaySelectAll={false}>
+                                    <TableRow>
+                                        <TableHeaderColumn>Order</TableHeaderColumn>
+                                        <TableHeaderColumn>Type</TableHeaderColumn>
+                                        <TableHeaderColumn>Name</TableHeaderColumn>
+                                        <TableHeaderColumn>Action</TableHeaderColumn>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody displayRowCheckbox={false} >
+                                    {tableData.map((row, index) => (
+                                        <TableRow key={index} selected={row.selected}>
+                                            <TableRowColumn>{row.order}</TableRowColumn>
+                                            <TableRowColumn>{row.type}</TableRowColumn>
+                                            <TableRowColumn>{row.name}</TableRowColumn>
+                                            <TableRowColumn><FlatButton icon={iconDelete} href="/" /></TableRowColumn>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                            <FlatButton label="Add" href="/" />
                         </Paper>
                     </div>
                 )
             case 2:
-                return (<AddProcessNotification />)
+                return (
+                    <div style={styles.block}>
+                        <Paper zDepth={1} style={styles.paper}>
+                            <br />
+                            <br />
+                            <Checkbox
+                                label="Notify Supervisor On Error"
+                                style={styles.checkbox}
+                                />
+                            <Checkbox
+                                label="Notify Supervisor On End"
+                                style={styles.checkbox}
+                                />
+                        </Paper>
+                    </div>
+                )
             case 3:
-                return (<AddProcessExceptionHandling />)
+                return (
+                    <div style={styles.block}>
+                        <Paper zDepth={1} style={styles.paper}>
+                            <br />
+                            <br />
+                            <Checkbox
+                                label="Assign to Supervisor On Error"
+                                style={styles.checkbox}
+                                />
+                            <Checkbox
+                                label="Block Process Execution"
+                                style={styles.checkbox}
+                                />
+                        </Paper>
+                    </div>
+                )
             default:
                 return 'Houston?'
         }
@@ -135,30 +238,27 @@ class AddProcessWizard extends Component {
     render() {
         const {finished, stepIndex} = this.state
         const contentStyle = { margin: '0 16px' }
-
         return (
             <MuiThemeProvider>
                 <div style={{ width: '100%', maxWidth: 700, margin: 'auto' }}>
                     <br /><br /><br />
                     <Stepper activeStep={stepIndex}>
                         <Step>
-                            <StepLabel>Insert Process Context</StepLabel>
+                            <StepLabel>Identification</StepLabel>
                         </Step>
                         <Step>
-                            <StepLabel>Insert Flow</StepLabel>
+                            <StepLabel>Flow</StepLabel>
                         </Step>
                         <Step>
-                            <StepLabel>Insert Notifications</StepLabel>
+                            <StepLabel>Notification</StepLabel>
                         </Step>
                         <Step>
-                            <StepLabel>Add Exception Handling</StepLabel>
+                            <StepLabel>Exception Handling</StepLabel>
                         </Step>
                     </Stepper>
                     <div style={contentStyle}>
-                        {finished ? (<p><a href="#" onClick={(event) => {
-                            event.preventDefault();
-                            this.setState({ stepIndex: 0, finished: false });
-                        } }>Click here </a> to reset the example.</p>
+                        {finished ? (
+                            alert('Process Saved')
                         ) : (
                                 <div>
                                     {this.getStepContent(stepIndex)}
