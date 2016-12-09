75722b797561485f23ef425bc5a3d9b190ea8971
leitediogo <leitediogo@gmail.com>
2016-12-09 16:02:58 +0000

    appbar homelink done

--- src/components/DecisionAppBar.js
+++ src/components/DecisionAppBar.js
@@ -3,24 +3,35 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
 import DecisionAppBarRightIconMenu from './DecisionAppBarRightIconMenu'
 import DecisionAppBarLeftIconMenu from './DecisionAppBarLeftIconMenu'
+import { browserHistory } from 'react-router'
 
-let style = {
-    position: "fixed",
-    top: "0px",
-    width: "100%",
-    backgroundColor: 'lightGrey'
+const styles = {
+    title: {
+        cursor: 'pointer'
+
+    },
+    bar: {
+        backgroundColor: 'lightGrey'
+    }
 }
 
 class DecisionAppBar extends Component {
+
+    handleAppBarClick() {
+        console.log('handleAppBarClick')
+        browserHistory.push('/')
+    }
+
     render() {
         return (
             <MuiThemeProvider>
                 <div>
                     <AppBar
-                        style={style}
-                        title="Flower"
+                        title={<span style={styles.title}>Flower</span>}
+                        style={styles.bar}
+                        onTitleTouchTap={this.handleAppBarClick}
                         iconElementRight={<DecisionAppBarRightIconMenu />}
-                        iconElementLeft={<DecisionAppBarLeftIconMenu filterDecisions={this.props.filterDecisions}/>}
+                        iconElementLeft={<DecisionAppBarLeftIconMenu filterDecisions={this.props.filterDecisions} />}
                         />
                 </div>
             </MuiThemeProvider>
